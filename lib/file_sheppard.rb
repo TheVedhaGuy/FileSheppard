60ffb347ae055607d829806fae22a5089d3665f2
Vedha Rajan <vedha.rajan@shopify.com>
2019-11-01 15:08:49 -0400

    Initial gem setup

---
+++ lib/file_sheppard.rb
@@ -0,0 +1,6 @@
+require "file_sheppard/version"
+
+module FileSheppard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/file_sheppard/version.rb
@@ -0,0 +1,3 @@
+module FileSheppard
+  VERSION = "0.1.0"
+end
